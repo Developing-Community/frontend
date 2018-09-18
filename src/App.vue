3b7195ea07f99d391d3010cd47867e249f4aabb1
Hosseinreza Akbari <hrakbari77@gmail.com>
2018-09-18 18:59:51 +0430

    ui container added to app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,31 @@
 
     <div>
 
-        <div class="ui pointing menu">
+        <div class="following bar">
+            
+            <div class="ui pointing menu">
 
-            <div class="right item ">
-                <a class="item" id = "Opener"> <i class="content icon"></i> </a>
-                <a class="item">گروهک</a>
-                <a class="item">تست</a>
+                <div class="right item ">
+                    <a class="item" id = "Opener"> <i class="content icon"></i> </a>
+                    <a class="item">گروهک</a>
+                    <a class="item">تست</a>
+                </div>
+            </div>
+
+            <div class="ui sidebar inverted vertical  menu">
+                <div class="right item"> Home </div>
+                <div class="item">Contact </div>
+                <div class="item">about </div>
+            </div> 
+            
+        </div>
+
+        <div>
+            <div class="ui container">
+            <router-view></router-view>
             </div>
         </div>
-
-        <div class="ui sidebar inverted vertical  menu">
-            <div class="right item"> Home </div>
-            <div class="item">Contact </div>
-            <div class="item">about </div>
-        </div>
-        <router-view></router-view>
+        
         <app-footer></app-footer>
     </div>
 
