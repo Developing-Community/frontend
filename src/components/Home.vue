634a9a07f595d26caba32ab1bbc22841cc1f94a1
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-09-24 19:08:56 +0330

    homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,25 +10,15 @@
         </div>
     </div>
 
-  <div class="ui vertical stripe segment" id="start">
+  <div class="ui vertical stripe segment" id="start" style="margin-top: 0px; padding-top:70px;">
     <div class="ui middle aligned stackable grid container">
+        <h4 class="ui horizontal header divider" style="text-align: center; width: 100%;">
+          <a style="font-size:140%;" href="#">یک کامیونیتی حرفه ای</a>
+        </h4>
       <div class="row">
         <div class="ten wide column center aligned">
           <h3 class="ui header">هدف ما</h3>
-          <p style="margin-top:30px;line-height:35px;">با توجه به نقدهای وارد بر نظام آموزشی فعلی و کافی نبودن تحصیلات برای پیشرفت در دنیای امروز، ایده این کامیونیتی در ذهنمان شکل گرفت
-            و تصمیم گرفتیم پلتفرمی ایجاد کنیم که ایده ها و استعدادهای شما را به ثمر برساند، بتوانید در کنار دیگر افراد یاد بگیرید و یاد بدهید و در فعالیت های مختلف همکاری کنید. منظور از فعالیت، هر فعالیتی در هر حوزه ای هست. 
-                    از یک تحقیق گروهی در علوم انسانی و پایه گرفته تا
-                        یک پروژه ی فیلم سازی یا برنامه نویسی
-          </p> 
-        </div>
-        <div class="four wide right floated column">
-          <img src="static/white-image.png" class="ui large bordered rounded image">
-        </div>
-      </div>
-      <div class="row">
-        <div class="nine wide column center aligned">
-          <h3 class="ui header">هدف ما</h3>
-          <p style="margin-top:30px;line-height:30px;">با توجه به نقدهای وارد بر نظام آموزشی فعلی و کافی نبودن تحصیلات برای پیشرفت در دنیای امروز، ایده این کامیونیتی در ذهنمان شکل گرفت
+          <p>با توجه به نقدهای وارد بر نظام آموزشی فعلی و کافی نبودن تحصیلات برای پیشرفت در دنیای امروز، ایده این کامیونیتی در ذهنمان شکل گرفت
             و تصمیم گرفتیم پلتفرمی ایجاد کنیم که ایده ها و استعدادهای شما را به ثمر برساند، بتوانید در کنار دیگر افراد یاد بگیرید و یاد بدهید و در فعالیت های مختلف همکاری کنید. منظور از فعالیت، هر فعالیتی در هر حوزه ای هست. 
                     از یک تحقیق گروهی در علوم انسانی و پایه گرفته تا
                         یک پروژه ی فیلم سازی یا برنامه نویسی
@@ -38,41 +28,69 @@
           <img src="static/white-image.png" class="ui large bordered rounded image">
         </div>
       </div>
-    </div>
-  </div>
 
-  <div class="ui vertical stripe segment">
-    <div class="ui middle aligned stackable grid container">
-      <div class="row">
-        <div class="six wide right floated column">
-          <img src="static/white-image.png" class="ui large bordered rounded image">
+        <h4 class="ui horizontal header divider">
+          <a href="https://docs.google.com/forms/d/1sGp9mHxO2yGs3Ij-NEozU9JefyIbySCoso96WLEAY8w" style="font-size:140%;">سیستم منتورینگ</a>
+        </h4>
+
+      <div class="row centered">
+        <div class="eight wide column center aligned">
+          <h3 class="ui header">یاد بگیرید، یاد بدهید</h3>
+          <p>
+            از افراد حرفه ای کامیونیتی راهنمایی بگیرید و مهارت و دانش مورد نیاز خود را کسب کنید. اطراف ما پر از انسان های مستعدی هستند که نیاز به
+                  مسیر یادگیری دارند. با تکیه بر دانش و
+                  مهارت خود به این افراد در کامیونیتی کمک کنید و همچنین نیروهای
+                  حرفه ای مورد نیاز خود را با صرف زمان اندک پرورش دهید.
+          </p>
+          <a class="ui large button" href="https://docs.google.com/forms/d/1sGp9mHxO2yGs3Ij-NEozU9JefyIbySCoso96WLEAY8w">اطلاعات بیشتر</a>
         </div>
-        <div class="eight wide column">
-          <h3 class="ui header">We Help Companies and Companions</h3>
-          <p>We can give your company superpowers to do things that they never thought possible. Let us delight your customers and empower your needs...through pure data analytics.</p>
-          <h3 class="ui header">We Make Bananas That Can Dance</h3>
-          <p>Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.</p>
+        <div class="eight wide column center aligned">
+          <h3 class="ui header">مسیر یادگیریتان را به اشتراک بگذارید</h3>
+          <p>
+            با به اشتراک‌گذاری مسیر یادگیریتان و تولید محتوای آموزشی، هم برای خودتان رزومه بسازید هم به یادگیری دیگران کمک کنید.
+            منابع یادگیری خود را ثبت و به آن ها امتیاز دهید و به دیگران کمک کنید منابع بهتری برای آموزش پیدا کرده و انتخاب کنند
+          </p>
         </div>
       </div>
-    </div>
-  </div>
 
+        <h4 class="ui horizontal header divider">
+          <a style="font-size:140%;">از ایده تا کار</a>
+        </h4>
 
-  <div class="ui vertical stripe segment">
-    <div class="ui middle aligned stackable grid container">
       <div class="row">
-        <div class="eight wide column">
-          <h3 class="ui header">We Help Companies and Companions</h3>
-          <p>We can give your company superpowers to do things that they never thought possible. Let us delight your customers and empower your needs...through pure data analytics.</p>
-          <h3 class="ui header">We Make Bananas That Can Dance</h3>
-          <p>Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.</p>
+        <div class="six wide column center aligned">
+          <h3 class="ui header">بازی با کلمات</h3>
+          <p dir="rtl">
+            ایده های خود را به اشتراک بگذارید و برای مشکلات خود راه حل بیابید. <span style="font-family: tahoma">brainstrom</span> جایی برای خلافیت شماست
+          </p> 
         </div>
-        <div class="six wide right floated column">
+        <div class="four wide floated column">
           <img src="static/white-image.png" class="ui large bordered rounded image">
         </div>
+        <div class="six wide column center aligned">
+          <h3 class="ui header">نیروی کار یا هم تیمی نیاز دارید؟</h3>
+          <p>
+                            برای انجام فعالیت های تیمی واقعی آماده شدید، 
+                        اگر توی کاری به هم تیمی هایی از حوزه
+                            خودتون یا حوزه های دیگه نیاز داشتید از طریق 
+                        این گروه سابقه گزارش ها و فعالیتشون رو ببینید و 
+                        با شناخت نسبی که پیدا کردید بهشون پیشنهاد بدید
+                            و نهایتا در قالب تیم با هم کار کنید.
+          </p> 
+        </div>
       </div>
+
+        <h4 class="ui horizontal header divider">
+          <a style="font-size:140%;">برای پیوستن به ما آماده اید؟</a>
+        </h4>
+      <div class="row centered">
+        <router-link :to="{name: 'register'}" tag="a" class="ui large button">ثبت نام و دریافت لینک گروه ها</router-link>
+        
+      </div>
+
     </div>
   </div>
+
 </div>
     <!-- <div class="ui container" style="text-align: right">
 
@@ -132,7 +150,7 @@
                 <hr>
                 <div>
                     <p>
-                        >اطراف ما پر از انسان های مستعدی هست که نیاز به
+                      اطراف ما پر از انسان های مستعدی هست که نیاز به
                             مسیر یادگیری دارند. با تکیه بر دانش و
                             مهارت خود به این افراد در کامیونیتی کمک کنید و همچنین نیروهای
                             حرفه ای مورد نیاز خود را با صرف زمان اندک پرورش دهید.
@@ -179,7 +197,10 @@ h5 {
   color: #2900a8;
   padding-top: 10px;
 }
-
+.column p {
+  margin-top: 30px;
+  line-height:35px;
+}
 .hidden.menu {
   display: none;
 }
