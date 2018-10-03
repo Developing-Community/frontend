6f177f0263f881787d17023c0c7a657d5a70822c
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-10-03 17:10:32 +0330

    sidebar hiding after route change

--- src/components/TelegramVerification.vue
+++ src/components/TelegramVerification.vue
@@ -2,13 +2,7 @@
 <div class="ui container" style="text-align: center; margin-bottom: 20px;">
     <div class="top_container ">
         <div class="col-sm-10" style="display:block; margin: auto;">
-                    
-            <div  v-if="loading" style="width: 100%; text-align: center;">
-                <img style="margin: auto;" src="/static/loading.gif" />
-            </div>
-            <div v-else>
-                
-            </div>
+            <img style="margin: auto;" src="/static/loading.gif" />
         </div>
     </div>
 </div>
@@ -19,16 +13,11 @@
     import { protectedUrlMixin } from "../protectedUrlMixin";
     
     export default {
-        data() {
-            return {
-                loading: false
-            };
-        },
         mixins: [protectedUrlMixin],
         mounted(){
-            this.loading = true;
             var vinst = this;
             if (this.$route.query.token) {
+                console.log(this.$store.state.hostUrl + '/api/user/auth/verify-telegram-token/');
                 axios.post(this.$store.state.hostUrl + '/api/user/auth/verify-telegram-token/', 
                 {
                     verify_token: this.$route.query.token
@@ -47,6 +36,8 @@
                     alert('خطا در تایید کد');
                     this.$router.push({ name: 'home'});
                 });
+            } else {
+                this.$router.push({ name: 'home'});
             }
         }
     }
